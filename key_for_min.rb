cb4e823542624c5c56896fac91b6853713bc5ff8
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 15:08:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = ""
-  value = 0
-  name_hash.collect do |key,value|
-
+  rkey = ""
+  rvalue = 1000
+  values.collect do |key,value|
+    if rvalue > value
+      rvalue = value
+      rkey = key
+    end
   end
-  return key
+  return rkey
 end

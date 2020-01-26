0a2afde06384daee44b4b8b36535620f27443d38
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 15:09:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   rkey = ""
   rvalue = 1000
+  return nil if name_hash.length == 0
   name_hash.collect do |key,value|
     if rvalue > value
       rvalue = value
